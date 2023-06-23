76e613c9d09ae9c2538237a01ca2895e94abf855
xuanet <the.dark.affliction@gmail.com>
2023-06-23 11:14:15 -0400

    Initial Commit

---
+++ WordleApp/WordleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleAppApp.swift
+//  WordleApp
+//
+//  Created by Kevin Xu on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct WordleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
